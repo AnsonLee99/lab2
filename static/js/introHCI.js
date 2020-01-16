b87be54c13188a6514e0beba858e4a0f93047b65
Anson <all197@ucsd.edu>
2020-01-16 12:43:22 -0800

    added Javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -6,4 +6,9 @@ function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
             });
+
+    $("a.thumbnail").click(function(e) {
+        e.preventDefault();
+        $(this).css("background-color", "#7fff00");
+    })
 }
